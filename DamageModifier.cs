59b221502f593cd1eb1722b7b2c08d4ccefd6dff
exkludera <51145038+exkludera@users.noreply.github.com>
2024-07-26 18:22:34 +0200

    Initial commit

---
+++ DamageModifier.cs
@@ -0,0 +1,109 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Memory;
+using CounterStrikeSharp.API.Modules.Memory.DynamicFunctions;
+using Microsoft.Extensions.Logging;
+using System.Runtime.InteropServices;
+using System.Text.Json.Serialization;
+
+public class Weapon
+{
+    public float ForceDamage { get; set; }
+    public float Multiplier { get; set; }
+}
+public class Config : BasePluginConfig
+{
+    [JsonPropertyName("Weapons")]
+    public Dictionary<string, Weapon> Weapons { get; set; } = new()
+    {
+        { "weapon_knife", new Weapon { Multiplier = 0.25f } },
+        { "weapon_ak47", new Weapon { Multiplier = 5.0f } },
+        { "weapon_awp", new Weapon { ForceDamage = 1.0f } },
+    };
+
+    [JsonPropertyName("Debug")]
+    public bool Debug { get; set; } = false;
+}
+
+public class DamageModifier : BasePlugin, IPluginConfig<Config>
+{
+    public override string ModuleName => "Damage Modifier";
+    public override string ModuleVersion => "1.0";
+    public override string ModuleAuthor => "exkludera";
+
+    public Config Config { get; set; } = new Config();
+    public void OnConfigParsed(Config config)
+    {
+        Config = config;
+
+        foreach (var weapon in Config.Weapons)
+        {
+            string weaponName = weapon.Key;
+            Weapon weaponConfig = weapon.Value;
+            Debug($"[DamageModifier] Weapon: {weaponName}, ForceDamage: {weaponConfig.ForceDamage}, Multiplier: {weaponConfig.Multiplier}");
+        }
+    }
+
+    public override void Load(bool hotReload)
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            VirtualFunctions.CBaseEntity_TakeDamageOldFunc.Hook(this.OnTakeDamage, HookMode.Pre);
+    }
+
+    public override void Unload(bool hotReload)
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            VirtualFunctions.CBaseEntity_TakeDamageOldFunc.Unhook(this.OnTakeDamage, HookMode.Pre);
+    }
+
+    HookResult OnTakeDamage(DynamicHook hook)
+    {
+        CEntityInstance ent = hook.GetParam<CEntityInstance>(0);
+        if (ent == null || !ent.IsValid || ent.DesignerName != "player")
+            return HookResult.Continue;
+
+        CTakeDamageInfo victimInfo = hook.GetParam<CTakeDamageInfo>(0);
+        var victim = victimInfo.As<CBasePlayerPawn>().Controller.Value;
+        if (victim == null || !victim.IsValid)
+            return HookResult.Continue;
+
+        CTakeDamageInfo damageInfo = hook.GetParam<CTakeDamageInfo>(1);
+        var attacker = damageInfo.Attacker.Value!.As<CBasePlayerPawn>().Controller.Value;
+        if (attacker == null || !attacker.IsValid)
+            return HookResult.Continue;
+
+        CCSWeaponBase? weaponData = damageInfo.Ability.Value?.As<CCSWeaponBase>();
+        if (weaponData == null || !weaponData.IsValid)
+            return HookResult.Continue;
+
+        if (Config.Weapons.ContainsKey(weaponData.DesignerName))
+        {
+            Weapon weaponConfig = Config.Weapons[weaponData.DesignerName];
+
+            var oldDamage = damageInfo.Damage;
+
+            var damage = oldDamage;
+
+            if (weaponConfig.ForceDamage > 0)
+                damage = weaponConfig.ForceDamage;
+
+            else if (weaponConfig.Multiplier > 0)
+                damage *= weaponConfig.Multiplier;
+
+            Debug($"[DamageModifier] attacker: {attacker.PlayerName}");
+            Debug($"[DamageModifier] victim: {victim!.PlayerName}");
+            Debug($"[DamageModifier] {weaponData.DesignerName} from {Math.Round(oldDamage)} to {Math.Round(damage)}");
+        }
+
+        return HookResult.Continue;
+    }
+
+    public void Debug(string message)
+    {
+        if (Config.Debug)
+        {
+            Server.PrintToChatAll(message);
+            Logger.LogDebug(message);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,31 @@
+# cs2-trails
+**a plugin to modify weapon damage**
+> request from counterstrikesharp discord
+
+<br>
+
+## information
+
+> [!CAUTION]
+> only runs on linux servers
+>
+> sometimes on fatal shots (according to the engine) breaks calculation? not sure how it works
+
+## config
+```json
+{
+  "Weapons": {
+    "weapon_knife": {
+      "Multiplier": 0.25
+    },
+    "weapon_ak47": {
+      "Multiplier": 5
+    },
+    "weapon_awp": {
+      "ForceDamage": 1
+    }
+  },
+  "Debug": false,
+  "ConfigVersion": 1
+}
+```
